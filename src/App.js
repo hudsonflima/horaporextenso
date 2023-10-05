e4fabe0e32f0e889c311016fb92c68be4987b079
Hudson Lima <hudsonflima@gmail.com>
2023-10-05 15:24:01 -0300

    Include footer + body slate-200

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles.css";
 import HoraExtenso from "./components/HoraExtenso";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <div className="App bg-slate-200">
       <Header />
       <HoraExtenso />
+      <Footer />
     </div>
   );
 }
